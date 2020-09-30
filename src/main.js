30659ba8c4d3a7905994aa6c1a7cabd71f336d10
Thien Phuc <ttran.phuc99@gmail.com>
2020-09-30 12:10:46 +0700

    add template

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import vuetify from '@/plugins/vuetify' // path to vuetify export
+import App from './App'
+import router from './router'
+// import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
-Vue.config.productionTip = false;
 
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
 new Vue({
+  el: '#app',
   router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+  vuetify,
+  components: { App },
+  template: '<App/>'
+})
--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,56 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Shop from '@/components/Shop'
+import Product from '@/components/Product'
+import Blog from '@/components/Blog'
+import Post from '@/components/Post'
+import Cart from '@/components/Cart'
+import Layout from '@/components/Layout'
 
-Vue.use(VueRouter);
+Vue.use(Router)
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: Layout,
+      children:[
+        {
+          path:'/',
+          component:Home,
+          name:'Home'
+        },
+        {
+          path:'/shop',
+          component:Shop,
+          name:'Shop'
+        },
+        {
+          path:'/product',
+          component:Product,
+          name:'Product'
+        },
+        {
+          path:'/blog',
+          component:Blog,
+          name:'Blog'
+        },
+        {
+          path:'/post',
+          component:Post,
+          name:'Post'
+        },
+        {
+          path:'/cart',
+          component:Cart,
+          name:'Cart'
+        }
+      ]
 
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
-});
+    }
+  ],
+    mode:'history'
+},
 
-export default router;
+  )
